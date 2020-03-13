36c12aaa1bf1973cf0078db4ff8642bdc968067b
Kausik N <nkausik1999@gmail.com>
2020-03-13 18:54:33 +0530

    changes

--- MemoryBasedComputation.py
+++ MemoryBasedComputation.py
@@ -7,12 +7,55 @@ import MemoryLibrary
 from tqdm import tqdm
 
 # Operation Methods
+def Multiply_MemoryBased(a, b, Memory, n): # Memory must have size 2^n x 2^n
+    ''' Multiplication Operation using memory method '''
+
+    modVal = (1 << n) - 1   # This is 2**n - 1      # a & modVal = a % len(Memory)
+
+    #print(a, "-", b)
+    # Check for zero
+    if a == 0 or b == 0:
+        return 0
+    if a == 1:
+        return b
+    elif b == 1:
+        return a
+
+    # Check Case
+    if a <= len(Memory) and b <= len(Memory):   # case = 1
+        #print("Case: 1")
+        val = Memory[a-1][b-1]
+        #print("1val1:", val)
+        return val
+
+    elif ((a <= len(Memory) and b > len(Memory)) or (a > len(Memory) and b <= len(Memory))):    # case = 2
+        #print("Case: 2")
+        x, y = max(a, b), min(a, b)
+        val = (Multiply_MemoryBased((x >> n), y, Memory, n) << n)
+        #print("2val1:", val)
+        val += Multiply_MemoryBased((x & modVal), y, Memory, n)
+        #print("2val2:", val)
+        return val
+
+    elif a > len(Memory) and b > len(Memory):   # case = 3
+        #print("Case: 3")
+        x, y = max(a, b), min(a, b)
+        val = (Multiply_MemoryBased((x >> n), (y >> n), Memory, n) << (2*n))
+        #print("3val1:", val)
+        val += (Multiply_MemoryBased((x >> n), (y & modVal), Memory, n) << n)
+        #print("3val2:", val)
+        val += (Multiply_MemoryBased((y >> n), (x & modVal), Memory, n) << n)
+        #print("3val3:", val)
+        val += Multiply_MemoryBased((x & modVal), (y & modVal), Memory, n)
+        #print("3val4:", val)
+        return val
+
 def Operation_MemoryBased(a, b, Memory, n, ZeroMemory): # Memory must have size 2^n x 2^n
     ''' Operation using memory method '''
 
     #print(a, "-", b)
     # Check for zero
-    if a == 0:
+    if a == 0 or b == 0:
         return ZeroMemory[1][b]
     elif b == 0:
         return ZeroMemory[0][a]
@@ -67,7 +110,7 @@ operandRange = (1, 2**n * 10)
 Memory = MemoryLibrary.CreateMemory_BasicOperation(2**n, operation)
 ZeroMemory = MemoryLibrary.CreateZeroMemory_BasicOperation(2**n, operation)
 
-# print("Ans:", Operation_MemoryBased(400, 256, Memory, n, ZeroMemory))
+# print("Ans:", Multiply_MemoryBased(570, 13, Memory, n))
 # quit()
 
 print("Creating Operands...")
@@ -81,13 +124,17 @@ print("Created Operands")
 print("Computing...")
 NormalOperation = []
 MemoryOperation = []
+notMatchCount = 0
 for i in tqdm(range(ntestcases)):
     NormalOperation.append(MemoryLibrary.BasicOperation(A[i], B[i], operation))
-    #print("Normal Operation:", A[i], "x", B[i], "=", NormalOperation[i])
-    MemoryOperation.append(Operation_MemoryBased(A[i], B[i], Memory, n, ZeroMemory))
-    #print("Memory Operation:", A[i], "x", B[i], "=", MemoryOperation[i])
+    print("Normal Operation:", A[i], "x", B[i], "=", NormalOperation[i])
+    MemoryOperation.append(Multiply_MemoryBased(A[i], B[i], Memory, n))
+    print("Memory Operation:", A[i], "x", B[i], "=", MemoryOperation[i])
     if not (NormalOperation[i] == MemoryOperation[i]):
         print("Not Matching:", A[i], operation, B[i], "= Norm:", NormalOperation[i], "Mem:", MemoryOperation[i])
+        notMatchCount += 1
+    #Multiply_MemoryBased(A[i], B[i], Memory, n)
+print("Not Matches:", notMatchCount)
 print("Computed")
 
 
